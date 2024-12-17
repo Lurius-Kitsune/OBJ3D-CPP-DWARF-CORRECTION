183c7a552cfe5d72029e0abf04b3dc7716490d4d
Lucas Bruel <81522727+Lurius-Kitsune@users.noreply.github.com>
2024-12-17 13:26:48 +0100

    -( magic finger

--- Level.cpp
+++ Level.cpp
@@ -1,17 +1,20 @@
 #include "Level.h"
 #include "Macro.h"
-#include "Filestream.h"
+#include "FileStream.h"
+#include "Emoji.h"
 
 Level::Level(const string& _path, Cursor* _cursor)
 {
-	savePath = "Asset/Level/" + _path;
+	path = "Assets/Levels/" + _path + ".txt";
 	LoadMap();
 	fullMapSize = Size(map);
-	viewSize = Size(35, 35);
+	view = Size(35, 35);
 	cursor = _cursor;
-
+	
+	//TODO move
 	Generate();
 }
+
 
 bool Level::IsOver(const Coords& _coords) const
 {
@@ -21,165 +24,163 @@ bool Level::IsOver(const Coords& _coords) const
 vector<string> Level::ConvertMapToString() const
 {
 	vector<string> _mapString;
+	const u_int& _rowsCount = static_cast<const u_int&>(map.size());
 
-	const u_int& _rowsCount = map.size();
-	for (u_int _row = 0; _row < _rowsCount; _row++)
+	for (u_int _rowIndex = 0; _rowIndex < _rowsCount; _rowIndex++)
 	{
-		const u_int& _colsCount = map[_row].size();
-		for (u_int _col = 0; _col < _colsCount; _col++)
+		const u_int& _columnsCount = static_cast<const u_int&>(map[_rowIndex].size());
+		string _rowText;
+
+		for (u_int _columnIndex = 0; _columnIndex < _columnsCount; _columnIndex++)
 		{
-			_mapString.push_back(map[_row][_col].ToString());
+			_rowText += map[_rowIndex][_columnIndex].ToString();
 		}
+
+		_mapString.push_back(_rowText);
 	}
 
 	return _mapString;
 }
 
+#pragma region Generation
+
+vector<Coords> Level::GetCoordsByBiome(const u_int& _biome) const
+{
+	vector<Coords> _availablesCoords;
+
+	const u_int& _mapSize = static_cast<const u_int&>(map.size());
+	for (u_int _rowIndex = 0; _rowIndex < _mapSize; _rowIndex++)
+	{
+		const u_int& _rowSize = static_cast<const u_int&>(map[_rowIndex].size());
+
+		for (u_int _columnIndex = 0; _columnIndex < _rowSize - 1; _columnIndex += 3)
+		{
+			if (map[_rowIndex][_columnIndex].GetBackgroundKey() == _biome)
+			{
+				_availablesCoords.push_back(Coords(_rowIndex, _columnIndex));
+			}
+		}
+	}
+
+	return _availablesCoords;
+}
+
+vector<Coords> Level::SelectCoords(vector<Coords> _availablesCooords, const u_int& _percentage) const
+{
+	const u_int& _coordsCount = static_cast<const u_int&>(_availablesCooords.size());
+	random_shuffle(_availablesCooords.begin(), _availablesCooords.end());
+	const u_int& _coordsCountToSelect = _percentage * _coordsCount / 100.0f;
+	vector<Coords> _selectedCoords;
+	_selectedCoords.insert(_selectedCoords.begin(), _availablesCooords.begin(), _availablesCooords.begin() + _coordsCountToSelect);
+	return _selectedCoords;
+}
+
+void Level::SpawnAtCoords(const vector<Coords>& _selectedCoords, const string& _elementToSpawn)
+{
+	for (const Coords& _coords : _selectedCoords)
+	{
+		map[_coords.x][_coords.y].SetAppearance(_elementToSpawn);
+	}
+}
 
 void Level::Generate()
 {
 	const string& _elementToSpawn = TREE;
 	const u_int& _percentage = 20;
 	const u_int& _biome = 2;
-	const vector<Coords>& _availableCoords = GetCoordsByBiome(_biome);
-	const vector<Coords>& _selectCoords = SelectCoords(_availableCoords, _percentage);
-	SpawnAtCoords(_selectCoords, _elementToSpawn);
+	const vector<Coords>& _availablesCooords = GetCoordsByBiome(_biome);
+	const vector<Coords>& _selectedCoords = SelectCoords(_availablesCooords, _percentage);
+	SpawnAtCoords(_selectedCoords, _elementToSpawn);
 }
 
-vector<Coords> Level::GetCoordsByBiome(const u_int& _biome) const
-{
-	vector<Coords> _availableCoords;
+#pragma endregion
 
-	const int _mapSize = static_cast<const int>(map.size());
-	for (int _rowIndex = 0; _rowIndex < _mapSize; _rowIndex++)
-	{
-		vector<Tile> _rowTile;
-		const int _rowSize = static_cast<const int>(map[_rowIndex].size());
-		for (int _columnIndex = 0; _columnIndex < _rowSize; _columnIndex += 3)
-		{
-			if (map[_rowIndex][_columnIndex].GetBackgroundKey() == _biome)
-			{
-				_availableCoords.push_back({_rowIndex, _columnIndex});
-			}
-		}
-	}
-	return _availableCoords;
-}
-
-vector<Coords> Level::SelectCoords(vector<Coords> _availableCoords, const u_int& _percentage) const
-{
-	const u_int& _coordsCount = static_cast<const u_int&>(_availableCoords.size());
-	random_shuffle(_availableCoords.begin(), _availableCoords.end());
-
-	const u_int& _coordsCountToSelect = _percentage * 100 / _coordsCount;
-	return vector<Coords>(_availableCoords.begin(), _availableCoords.begin() + _coordsCountToSelect);
-
-}
-
-void Level::SpawnAtCoords(const vector<Coords>& _selectCoords, const string& _elementToSpawn)
-{
-	for (const Coords& _coords : _selectCoords)
-	{
-		map[_coords.x][_coords.y].SetAppearance(_elementToSpawn);
-	}
-}
+#pragma region Save
 
 void Level::LoadMap()
 {
-
-	const vector<string>& _allMap = FileStream::ReadAll(savePath);
-	string _tileText;
-
+	const vector<string>& _allMap = FileStream::ReadAll(path);
 	const u_int& _mapSize = static_cast<const u_int&>(_allMap.size());
+
 	for (u_int _rowIndex = 0; _rowIndex < _mapSize; _rowIndex++)
 	{
 		vector<Tile> _rowTile;
 		const u_int& _rowSize = static_cast<const u_int&>(_allMap[_rowIndex].size());
-		for (u_int _columnIndex = 0; _columnIndex < _rowSize; _columnIndex+=3)
+
+		for (u_int _columnIndex = 0; _columnIndex < _rowSize - 1; _columnIndex += 3)
 		{
-			for (u_int _i = 0; _i < 3; _i++)
+			string _tileText;
+			for (u_int _index = 0; _index < 3; _index++)
 			{
-				_tileText += _allMap[_rowIndex][_columnIndex + _i];
+				_tileText += _allMap[_rowIndex][_columnIndex + _index];
 			}
 			_rowTile.push_back(Tile(_tileText));
 		}
-		map.push_back(_rowTile);
 
+		map.push_back(_rowTile);
 	}
 }
-
 void Level::SaveMap()
 {
-
+	FileStream::Write(path, ConvertMapToString());
 }
-
-
-bool Level::IsValidCoords(const u_int& _rowIndex, const u_int& _columnIndex) const
-{
-	return _rowIndex >= 0 && _rowIndex < fullMapSize.sizeX
-		&& _columnIndex >= 0 && _columnIndex < fullMapSize.sizeY;
-}
-
-void Level::DisplayMap(const Size& _size, const Coords& _start)const
-{
-	const u_int& _mapSize = static_cast<const u_int&>(_size.sizeX);
-	for (u_int _rowIndex = 0; _rowIndex < _mapSize; _rowIndex++)
-	{
-		const u_int& _rowSize = static_cast<const u_int&>(_size.sizeY);
-		for (u_int _columnIndex = 0; _columnIndex < _rowSize ; _columnIndex++)
-		{
-			const u_int& _posX = _rowIndex + _start.x;
-			const u_int& _posY = _columnIndex + _start.y;
-			if (!IsValidCoords(_rowIndex, _columnIndex))
-			{
-				continue;
-			}
-
-			map[_posX][_posY].Display();
-
-			const bool _isCursor = cursor->GetCoords() == Coords(_posX, _posY);
-
-			string _tileText;
-			for (u_int _i = 0; _i < 3; _i++)
-			{
-				_tileText += map[_posX + _i][_posY].ToString();
-			}
-			Tile(_tileText).Display();
-
-			if (_isCursor)
-			{
-				cout << cursor->GetAppearance();
-			}
-		}
-		cout << endl;
-	}
-	cout << _start.ToString() << endl;
-	cout << cursor->GetCoords().ToString() << endl;
-}
-
-Coords Level::ComputeCenter(const Coords& _cursorPos) const
-{
-	return
-	{
-		static_cast<int>(_cursorPos.x - viewSize.sizeX / 2),
-		static_cast<int>(_cursorPos.y - viewSize.sizeY / 2)
-	};
-}
-
-
 void Level::Save()
 {
 	SaveMap();
 }
 
-void Level::DisplayView(const Coords& _coords)
+#pragma endregion
+
+#pragma region Display
+
+void Level::DisplayMap(const Size& _size, const Coords& _start) const
 {
-	DisplayMap(viewSize, ComputeCenter(_coords));
+	cursor->SetCursorPosition(0, 0);
+
+	const u_int& _mapSize = static_cast<const u_int&>(_size.x);
+	for (u_int _rowIndex = 0; _rowIndex < _mapSize; _rowIndex++)
+	{
+		const u_int& _rowSize = static_cast<const u_int&>(_size.y);
+		for (u_int _columnIndex = 0; _columnIndex < _rowSize; _columnIndex++)
+		{
+			const u_int& _posX = _rowIndex + _start.x;
+			const u_int& _posY = _columnIndex + _start.y;
+			if (!IsValidCoords(_posX, _posY)) continue;
+
+			map[_posX][_posY].Display();
+			const bool _isCursor = cursor->GetCoords() == Coords(_posY, _posX);
+			if (_isCursor)
+			{
+				Print("", cursor->GetAppearance());
+			}
+		}
+
+		cout << endl;
+	}
 }
 
-void Level::DisplayFullMap()
+Coords Level::ComputeCenter(const Coords& _cursorPos) const
+{
+	return {
+		 _cursorPos.x - view.x / 2,
+		 _cursorPos.y - view.y / 2,
+	};
+}
+
+bool Level::IsValidCoords(const u_int& _rowIndex, const u_int& _columnIndex) const
+{
+	return _rowIndex >= 0 && _rowIndex < (u_int)fullMapSize.x
+		&& _columnIndex >= 0 && _columnIndex < (u_int)fullMapSize.y;
+}
+
+void Level::DisplayView(const Coords& _cursorPos) const
+{
+	DisplayMap(view, ComputeCenter(_cursorPos));
+}
+
+void Level::DisplayFullMap() const
 {
 	DisplayMap(fullMapSize);
 }
 
-
+#pragma endregion
--- Tile.cpp
+++ Tile.cpp
@@ -1,101 +1,64 @@
 #include "Tile.h"
+#include "Color.h"
+#include "Macro.h"
 
 Tile::Tile(const string& _key)
 {
-    key = _key;
-    keyValue = stoi(key);
-    emojis = 
-    {
-        VILLAGE,
-        HOUSE,
-        FORGE,
-        MONUMENT,
-        HUNTER_HUT,
-        LOVE_HOUSE,
-        TRACTOR,
-        FARM_EMOJI,
-        ANIMAL,
-        WOLF,
-        BOAR,
-        FISH,
-        MONKEY,
-        CRAB,
-        TIGER,
-        LION,
-        CROCODILE,
-        PARROT,
-        GORILLA,
-        WATER,
-        TREE,
-        TREE2,
-        PALM_TREE,
-        GRASS,
-        WIND,
-        FLOWER1,
-        FLOWER2,
-        FLOWER3,
-        FLOWER4,
-        ROCK,
-        FOOD,
-        LOG,
-        MEAT,
-        DWARF,
-        ENEMY,
-        FOOTSTEP
-    };
+	key = stoi(_key);
 }
+
 
 string Tile::ComputeColor() const
 {
-    const Color _colors[] =
-    {
-        Color(0,4,217),
-        Color(254,255,100),
-        Color(20,185,20),
-        Color(88,41,0),
-        Color(56,56,56),
-        Color(244,102,27),
-        Color(255,0,0),
-    };
+	const Color _colors[] = {
 
-    const u_int& _backgroundKey = keyValue % 10;
-    string _color = _colors[_backgroundKey].ToString(false);
-    return _color += HasEmoji() ? "" : " ";
+		Color(0,4,217), // Bleu
+		Color(254,255,100), // Jaune
+		Color(20,185,20), // Vert
+		Color(88,41,0), // Marron
+		Color(56,56,56), // Gris
+		Color(244,102,27), // Orange 
+		Color(255,0,0), // Rouge
+	};
+	const u_int& _backgroundKey = GetBackgroundKey();
+	string _color = _colors[_backgroundKey].ToString(false);
+	return _color += HasEmoji() ? "" : "  ";
 }
 
 string Tile::ComputeAppearance() const
 {
-    if (!HasEmoji()) return "";
-    
-
-    const u_int& _forwardKey = keyValue / 10 - 1;
-    return emojis[_forwardKey];
-}
-
-void Tile::Display() const
-{
-    Print("", ComputeColor());
-    if (HasEmoji()) Print("", ComputeAppearance());
-}
-
-string Tile::ToString() const
-{
-    return to_string(keyValue);
+	const u_int& _forwardKey = GetForwardKey() - 1;
+	return emojis[_forwardKey];
 }
 
 u_int Tile::GetKeyByAppearance(const string& _appearance) const
 {
-    u_int _index = 0;
-    for (const string& _emoji : emojis)
-    {
-        if (_emoji == _appearance) return _index;
-        _index++;
-    }
+	u_int _index = 0;
 
-    throw exception("ERROR => Invalid key for appearance");
+	for (const string& _currentEmoji : emojis)
+	{
+		if (_currentEmoji == _appearance) return _index + 1;
+		_index++;
+	}
+
+	throw exception("ERROR => Invalid key appearance !");
 }
 
 void Tile::SetAppearance(const string& _appearance)
 {
-    key = GetKeyByAppearance(_appearance);
+	key += 10 * GetKeyByAppearance(_appearance);
+}
+
+void Tile::Display() const
+{
+	Print("", ComputeColor());
+	if (HasEmoji())
+	{
+		Print("", ComputeAppearance());
+	}
+}
+
+string Tile::ToString() const
+{
+	return to_string(key);
 }
