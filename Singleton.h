3d8e0a06c7649e944fcc68f450f97a59ecc0ee04
Lucas Bruel <81522727+Lurius-Kitsune@users.noreply.github.com>
2024-12-19 14:22:10 +0100

    Singleton

--- Singleton.h
+++ Singleton.h
@@ -1,8 +1,17 @@
 #pragma once
+#include "CoreMinimal.h"
+
+template <typename Class>
 class Singleton
 {
-	static 
+protected:
+	static Class instance;
 
+public: 
+	static Inline Class& GetInstance()
+	{
+		return instance;
+	}
 public:
 	Singleton();
 	~Singleton();
