0ce2f38302b3319000cda10970c88b319831f07e
kaunewy <yoyobas2004@gmail.com>
2024-12-19 09:45:09 +0100

    j

---
+++ Dwarf.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Entity.h"
+class Dwarf : public Entity
+{
+public:
+	Dwarf();
+};
+
---
+++ Entity.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Component.h"
+class Entity
+{
+public:
+	Entity();
+};
+
