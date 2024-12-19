9c585bee4fb0b56e9d4951c53cd62c242ca70b08
Lucas Bruel <81522727+Lurius-Kitsune@users.noreply.github.com>
2024-12-19 15:00:01 +0100

    - start game

--- StartMenu.cpp
+++ StartMenu.cpp
@@ -4,11 +4,12 @@
 StartMenu::StartMenu() : Menu("Start")
 {
 	allSaves = vector<Save>(3);
-	allSaves[0] = Save("Save1", "coucou");
+	allSaves[0] = Save("Save1", "MainLevel");
 }
 
 void StartMenu::LaunchGame(const string& _path)
 {
+	// Load Save Path
 	Level* _level = new Level(_path);
 	Game::GetInstance().Start(_level);
 }
@@ -17,16 +18,6 @@ void StartMenu::Show(const bool _toAdd)
 {
 	function<void(const u_int&)> _callback = [&](const u_int& _index) { LaunchGame(allSaves[_index].path); };
 	OpenMenu(allSaves, "Quel sauvegarde choisissez-vous ?", _callback, false);
-
-	u_int _index = 0;
-	for (const Save& _save : allSaves)
-	{
-		cout << _index++ << " - " << _save.title << endl;
-	}
-
-	u_int _textIndex;
-	cin >> _textIndex;
-	_textIndex--;
 }
 
 void StartMenu::Hide()
